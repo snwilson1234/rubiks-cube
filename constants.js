b3c70530f3a6483c228951f605af3c57cbbc83cc
Shelby <snwilson0502@gmail.com>
2025-07-27 18:00:15 -0400

    move constants to separate file

---
+++ constants.js
@@ -0,0 +1,92 @@
+import * as THREE from 'three';
+
+
+/* Constants and Helpers */
+const CUBELET_SIZE = 0.95;
+const CUBE_SPACING = 1.05;
+const CUBE_COLORS = {
+  red: 0xd11515,
+  orange: 0xde7309,
+  yellow: 0xffcf0d,
+  green: 0x06cc21,
+  blue: 0x425af5,
+  purple: 0x8706cc,
+  black: 0x000000,
+  white: 0xffffff
+};
+const mat = (colorName) => new THREE.MeshBasicMaterial({ color: CUBE_COLORS[colorName] });
+const makeRow = (faces) => faces.map(row => row.map(mat));
+const CUBE_MATERIALS = {
+  firstRow: {
+    corners: makeRow([
+      ['black','red','black','white','black','blue'],
+      ['black','red','black','white','green','black'],
+      ['black','red','yellow','black','black','blue'],
+      ['black','red','yellow','black','green','black']
+    ]),
+    sides: makeRow([
+      ['black','red','black','white','black','black'],
+      ['black','red','black','black','black','blue'],
+      ['black','red','black','black','green','black'],
+      ['black','red','yellow','black','black','black']
+    ]),
+    center: [
+      new THREE.MeshBasicMaterial({ color: CUBE_COLORS['black'] }),
+      new THREE.MeshBasicMaterial({ color: CUBE_COLORS['red'] }),
+      new THREE.MeshBasicMaterial({ color: CUBE_COLORS['black'] }),
+      new THREE.MeshBasicMaterial({ color: CUBE_COLORS['black'] }),
+      new THREE.MeshBasicMaterial({ color: CUBE_COLORS['black'] }),
+      new THREE.MeshBasicMaterial({ color: CUBE_COLORS['black'] })
+    ]
+  },
+  secondRow: {
+    corners: makeRow([
+      ['black','black','black','white','black','blue'],
+      ['black','black','black','white','green','black'],
+      ['black','black','yellow','black','black','blue'],
+      ['black','black','yellow','black','green','black']
+    ]),
+    sides: makeRow([
+      ['black','black','black','white','black','black'],
+      ['black','black','black','black','black','blue'],
+      ['black','black','black','black','green','black'],
+      ['black','black','yellow','black','black','black']
+    ]),
+    center: [
+      new THREE.MeshBasicMaterial({ color: CUBE_COLORS['black'] }),
+      new THREE.MeshBasicMaterial({ color: CUBE_COLORS['black'] }),
+      new THREE.MeshBasicMaterial({ color: CUBE_COLORS['black'] }),
+      new THREE.MeshBasicMaterial({ color: CUBE_COLORS['black'] }),
+      new THREE.MeshBasicMaterial({ color: CUBE_COLORS['black'] }),
+      new THREE.MeshBasicMaterial({ color: CUBE_COLORS['black'] })
+    ]
+  },
+  thirdRow: {
+    corners: makeRow([
+      ['orange','black','black','white','black','blue'],
+      ['orange','black','black','white','green','black'],
+      ['orange','black','yellow','black','black','blue'],
+      ['orange','black','yellow','black','green','black']
+    ]),
+    sides: makeRow([
+      ['orange','black','black','white','black','black'],
+      ['orange','black','black','black','black','blue'],
+      ['orange','black','black','black','green','black'],
+      ['orange','black','yellow','black','black','black']
+    ]),
+    center: [
+      new THREE.MeshBasicMaterial({ color: CUBE_COLORS['orange'] }),
+      new THREE.MeshBasicMaterial({ color: CUBE_COLORS['black'] }),
+      new THREE.MeshBasicMaterial({ color: CUBE_COLORS['black'] }),
+      new THREE.MeshBasicMaterial({ color: CUBE_COLORS['black'] }),
+      new THREE.MeshBasicMaterial({ color: CUBE_COLORS['black'] }),
+      new THREE.MeshBasicMaterial({ color: CUBE_COLORS['black'] })
+    ]
+  }
+};
+
+export {
+  CUBELET_SIZE,
+  CUBE_MATERIALS,
+  CUBE_SPACING
+};
